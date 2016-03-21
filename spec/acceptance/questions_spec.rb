f1eee4b87b748af4646a20d508b2150f2acf3c85
Maxim Portnyagin <maks.portnyagin@gmail.com>
2016-03-21 21:21:15 +0700

    Add index of questions

--- spec/acceptance/questions_spec.rb
+++ spec/acceptance/questions_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
+feature 'User can view all question', %q{
+  In order to be view all question
+  As an user
+  I want to go to index page
+} do
+
+  given(:questions) { create_list(:question, 5) }
+  before { questions }
+
+  scenario 'view all question' do
+    visit root_path
+
+    questions.each do |question|
+      expect(page).to have_content question.title
+      expect(page).to have_content question.body
+    end
+  end
+
+end
+
 feature 'Create new question', %q{
   In order to be able to ask question
   As an user
