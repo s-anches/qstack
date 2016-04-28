e9c85bb10efbc3f1abd368db93eeabe805251bfb
Maxim Portnyagin <maks.portnyagin@gmail.com>
2016-04-28 19:36:20 +0700

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
-  resources :users, only: :show do
-  end
+  resources :users, only: :show
 
   root 'questions#index'
 
