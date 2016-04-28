7ffdebc7410241d4ce94c7877df8a06b934cfd66
Maxim Portnyagin <maks.portnyagin@gmail.com>
2016-04-28 23:03:35 +0700

    Add to API V1 (GET profiles/)

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -7,6 +7,10 @@ class Api::V1::ProfilesController < ApplicationController
   def me
     respond_with current_resource_owner
   end
+  
+  def index
+    respond_with User.where.not(id: current_resource_owner)
+  end
 
   protected
     def current_resource_owner
--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -37,4 +37,48 @@ describe 'Profile API' do
       end
     end
   end
+
+  describe 'GET /' do
+    context 'authorized' do
+      let(:me) { create(:user) }
+      let!(:others) { create_list(:user, 5) }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+
+      before { get '/api/v1/profiles', format: :json, access_token: access_token.token }
+
+      it 'return 200 status' do
+        expect(response).to be_success
+      end
+
+      it 'does not contain me' do
+        expect(response.body).to_not include_json(me.to_json)
+      end
+
+      it "contain profiles" do
+          expect(response.body).to have_json_path("profiles")
+      end
+
+      it 'contains all other index' do
+        expect(response.body).to include_json(others.to_json)
+      end
+
+      %w(password encrypted_password).each do |attr|
+        it "does not contain #{attr}" do
+          expect(response.body).to_not have_json_path(attr)
+        end
+      end
+    end
+
+    context 'unauthorized' do
+      it 'return 401 status if there is no access token' do
+        get '/api/v1/profiles', format: :json
+        expect(response.status).to eq 401
+      end
+
+      it 'return 401 status if access token is invalid' do
+        get '/api/v1/profiles', format: :json, access_token: '1234'
+        expect(response.status).to eq 401
+      end
+    end
+  end
 end
