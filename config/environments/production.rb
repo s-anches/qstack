d0e7f608fcd3dd020724aad9038fe65cb7c69da3
Maxim Portnyagin <maks.portnyagin@gmail.com>
2016-05-29 20:27:56 +0700

    Change mail system to sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   config.action_mailer.default_url_options = { host: 'portnyagin.space' }
-  #config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default :charset => "utf-8"
 
