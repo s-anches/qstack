05acedea296ab8b45bd043253d22304bbe17db1e
Maxim Portnyagin <maks.portnyagin@gmail.com>
2016-06-03 14:53:23 +0700

    Fix nil send question digest

--- app/mailers/daily_mailer.rb
+++ app/mailers/daily_mailer.rb
@@ -2,6 +2,6 @@ class DailyMailer < ApplicationMailer
   def digest(user)
     @user = user
     @questions = Question.digest
-    mail(to: @user.email, subject: 'Every day digest!')
+    mail(to: @user.email, subject: 'Every day digest!') if @questions.presence
   end
 end
